eadae674ba9fda3e18b9a6a0a5321d55daeb81eb
bilalhadid <hadid0648@gmail.com>
2020-10-09 03:13:18 +0500

    Home

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import React from "react";
 import "./App.css";
 import Launch from "./components/Launch";
 import Launchdetail from "./components/LaunchDetails";
+import { Home } from "./components/Home/Home";
 
 const App = () => {
   return (
     <div className="Ap">
+      <Home />
       <Launch />
       <Launchdetail />
     </div>
---
+++ src/components/Home/Home.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import img1 from "../../assets/Images/space-x-image.jpg";
+import "./Home.css";
+
+export const Home = () => {
+  return (
+    <div className="Home">
+      <h1>Helo</h1>
+    </div>
+  );
+};
