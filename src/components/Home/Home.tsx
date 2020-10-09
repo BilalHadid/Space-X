fc5e806d15c1a3cdbbf9b733228795c10ff09b7f
bilalhadid <hadid0648@gmail.com>
2020-10-10 02:19:56 +0500

    All

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,10 +1,33 @@
 import React from "react";
+import logo from "../../assets/Images/logo.png";
+import rocket from "../../assets/Images/rocket.png";
+
 import "./Home.css";
 
 export const Home = () => {
   return (
     <div className="Home">
-      <h1>Helo</h1>
+      {/* <img src={logo} alt="Logo" /> */}
+
+      {/* <div style={{ marginTop: "40%" }}>
+        <span className="scroll-btn">
+          <span className="mouse">
+            <span></span>
+          </span>
+        </span>
+      </div> */}
+      <div>
+        <img
+          src={rocket}
+          alt="rocket"
+          style={{
+            height: "50%",
+            width: "50% ",
+            marginTop: "3%",
+            marginLeft: "40%",
+          }}
+        />
+      </div>
     </div>
   );
 };
