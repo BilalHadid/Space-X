df812374a5b72c21c0f6af36b32b69d882b9b23e
bilalhadid <hadid0648@gmail.com>
2020-10-12 02:17:36 +0500

    All

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -5,29 +5,5 @@ import rocket from "../../assets/Images/rocket.png";
 import "./Home.css";
 
 export const Home = () => {
-  return (
-    <div className="Home">
-      {/* <img src={logo} alt="Logo" /> */}
-
-      {/* <div style={{ marginTop: "40%" }}>
-        <span className="scroll-btn">
-          <span className="mouse">
-            <span></span>
-          </span>
-        </span>
-      </div> */}
-      <div>
-        <img
-          src={rocket}
-          alt="rocket"
-          style={{
-            height: "50%",
-            width: "50% ",
-            marginTop: "3%",
-            marginLeft: "40%",
-          }}
-        />
-      </div>
-    </div>
-  );
+  return <div className="Home"></div>;
 };
