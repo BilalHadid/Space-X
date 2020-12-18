ffc15cd52d4156aecb78c39ddeebe6234ed83537
bilalhadid <hadid0648@gmail.com>
2020-12-18 23:15:56 +0500

    Renew

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import logo from "../../assets/Images/logo.png";
-import rocket from "../../assets/Images/rocket.png";
+// import logo from "../../assets/Images/logo.png";
+// import rocket from "../../assets/Images/rocket.png";
 
 import "./Home.css";
 
