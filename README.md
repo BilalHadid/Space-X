11eb976d40018d25dbb2c390c550c76f437e40d7
bilalhadid <hadid0648@gmail.com>
2020-12-21 00:45:58 +0500

    ALL Completed

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+## For Live Review
+
+[Click](https://spacex-1d024.web.app/).
+
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
---
+++ public_/build/precache-manifest.5539a3829b58e99c7ae086a8d79e2d23.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fceb43cde5f703c3e4524f8a9660f9e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82f40cfa824cd8a13981",
+    "url": "/static/css/main.be8b2a63.chunk.css"
+  },
+  {
+    "revision": "4900e73eff72e9cfb238",
+    "url": "/static/js/2.ec88e03d.chunk.js"
+  },
+  {
+    "revision": "1937a0fc6634269e5f47f8181e27fdc7",
+    "url": "/static/js/2.ec88e03d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82f40cfa824cd8a13981",
+    "url": "/static/js/main.c44bd6eb.chunk.js"
+  },
+  {
+    "revision": "5268b1c7be79030bb5b5",
+    "url": "/static/js/runtime-main.142b1330.js"
+  },
+  {
+    "revision": "37a39b35e699355acde6016e5ce69039",
+    "url": "/static/media/logo1.37a39b35.png"
+  }
+]);
